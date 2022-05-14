09a3936b05905d122fbebd69bea798aced54b484
Benedek Árvay <arvaybenedek@edu.bme.hu>
2022-05-14 17:59:38 +0200

    asd

--- VakVirologusok_proto/src/Game.java
+++ VakVirologusok_proto/src/Game.java
@@ -7,21 +7,26 @@ import java.util.List;
 public class Game
 {
     private int nPlayers;
-    private int numOfAgents;
     private List<Virologist> playersInGame;
     private List<Field> fieldsInGame;
 
     /**
-    * Leírás:
-    * Game konstruktora
+    * Leírás: pályaelemek létrhozása
     */
     public void StartGame()
     {
-        for(int i = 0; i < nPlayers; i++) {
+        for(int i = 0; i < nPlayers; i++) { // annyi raktár és óvóhely ahány játékos, +1 field
             fieldsInGame.add(new Field());
+            fieldsInGame.add(new Shelter());
+            fieldsInGame.add(new Warehouse());
         }
-        for(int i = 0; i < 4; i++) {
+
+        for(int i = 0; i < 4; i++) { // 4db labor, különböző ágensekkel
             fieldsInGame.add(new Laboratory(i));
+        }
+
+        for (Virologist v : playersInGame) { // minden játékos a kezdő mezőre
+            v.SetInitialField(fieldsInGame.get(0));
         }
     }
     
@@ -40,16 +45,14 @@ public class Game
     */
     public void SetFields()
     {
+        Field last = new Field();
+        last.AddNeighbour(fieldsInGame.get(fieldsInGame.size() -1));
+        fieldsInGame.get(fieldsInGame.size() -1).AddNeighbour(last);
 
-    }
-
-    /**
-    * Leírás:
-    * visszadja a numOfAgents tagváltozó értékét
-    * @return int numOfAgents
-    */
-    public int GetNumOfAgents()
-    {
-        return numOfAgents;
+        for(Field current : fieldsInGame) {
+            last.AddNeighbour(current);
+            current.AddNeighbour(last);
+            last = current;
+        }
     }
 }
