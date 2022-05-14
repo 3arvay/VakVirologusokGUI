fd2a72fd4559077e7aa8f48d8e0ef19cad46b7ff
Benedek Árvay <arvaybenedek@edu.bme.hu>
2022-05-14 19:42:01 +0200

    StartGame, SetFields

--- VakVirologusok_proto/src/Main.java
+++ VakVirologusok_proto/src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
+
         Game g = new Game();
-        // TODO plyercount bekérése itt (Game:nPlayers)
         g.StartGame();
-        g.SetFields();
-        System.out.println("The Game is On");
     }
 }
