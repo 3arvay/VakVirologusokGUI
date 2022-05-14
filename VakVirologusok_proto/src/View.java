7b83b5a0de1d6a08c05df92e0c88e418989355e4
Fabs <nemeth.a.fabian@gmail.com>
2022-05-14 18:35:02 +0200

    2nd

---
+++ VakVirologusok_proto/src/View.java
@@ -0,0 +1,67 @@
+/**
+ * Leírás:
+ * Létrehozza .
+ */
+public class View
+{
+
+    public void WinDialogShow()
+    {
+
+    }
+
+    public void CraftDialogShow()
+    {
+
+    }
+
+    public void AttackDialogShow()
+    {
+
+    }
+
+    public void StealDialogShow()
+    {
+
+    }
+
+    public void DropDialogShow()
+    {
+
+    }
+
+    public void DrawAll()
+    {
+
+    }
+
+    public void DrawField()
+    {
+
+    }
+
+    public void DrawVirologist()
+    {
+
+    }
+
+    public void DrawGenCode()
+    {
+
+    }
+
+    public void DrawAgent()
+    {
+
+    }
+
+    public void DrawVAttribute()
+    {
+
+    }
+
+    public void DrawGear()
+    {
+
+    }
+}
